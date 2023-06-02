91c7b8cc46bb02d0d4bf14eaf04d17c0f85ff1c7
kikingke <eabucam@gmail.com>
2023-06-02 17:35:11 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterView } from "vue-router";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { almacen } from './stores/auth.js'
+
+const datos = almacen()
+
+const auth = getAuth();
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const uid = user.uid;
+    datos.isLoggedIn = true;
+    console.log('Usuario Logueado')
+  } else {
+    datos.isLoggedIn = false;
+    console.log('Usuario NO Logueado')
+  }
+});
+
+</script>
+
+<template>
+  <div>
+    <RouterView />
+  </div>
+</template>
