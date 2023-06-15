80c6523f9e59432a07f84f110e1015f7148f491d
stivenarias229 <stivenarias229@gmail.com>
2023-06-14 19:43:53 -0500

    Update

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	trailingComma: 'none',
+	tabWidth: 4,
+	singleQuote: true,
+	useTabs: true,
+	bracketSpacing: true,
+	bracketSameLine: true,
+	arrowParens: 'avoid',
+	proseWrap: 'always',
+	endOfLine: 'lf',
+	semi: false,
+	vueIndentScriptAndStyle: true,
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,48 @@
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { createRouter, createWebHistory } from 'vue-router'
 
-
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: 'Home',
-      component: () => import("../views/Home.vue"),
-    },
-    {
-      path: "/about",
-      name: 'About',
-      component: () => import("../Views/About.vue"),
-      meta: { requiresAuth: true }
-        
-    },
-  ]
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '/',
+			name: 'Home',
+			component: () => import('../views/Home.vue')
+		},
+		{
+			path: '/about',
+			name: 'About',
+			component: () => import('../Views/About.vue'),
+			meta: { requiresAuth: true }
+		}
+	]
 })
 
 const getCurrentUser = () => {
-  return new Promise(( resolve, reject ) =>{
-    const removeListener = onAuthStateChanged(
-      getAuth(),
-      (user) => {
-        removeListener();
-        resolve(user)
-      },
-      reject
-    )
-  })
+	return new Promise((resolve, reject) => {
+		const removeListener = onAuthStateChanged(
+			getAuth(),
+			user => {
+				removeListener()
+				resolve(user)
+			},
+			reject
+		)
+	})
 }
 
-router.beforeEach( async (to, from, next) => {
-  if (to.matched.some((record) => record.meta.requiresAuth)){
-    if (await getCurrentUser()){
-      next();
-    } else {
-      swal('No access!!!');
-      
-      next('/');
-    }
-  } else {
-    next();
-  }
-});
+router.beforeEach(async (to, from, next) => {
+	if (to.matched.some(record => record.meta.requiresAuth)) {
+		if (await getCurrentUser()) {
+			next()
+		} else {
+			swal('No access!!!')
+
+			next('/')
+		}
+	} else {
+		next()
+	}
+})
 
 export default router
